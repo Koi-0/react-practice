1f7dd661334f6629c41c1a6eacc57bf9d279ff61
Koi-0 <dndud5182@gmail.com>
2025-01-22 17:43:12 +0900

    map 활용 및 1차 검증 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 컴포넌트 밖, 필요한  파일 가져오는 영역
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    // JavaScript 영역
+    const [value, setValue] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const [lists, setList] = useState([
+        { id: crypto.randomUUID(), text: "국가명" },
+        { id: crypto.randomUUID(), text: "금메달" },
+        { id: crypto.randomUUID(), text: "은메달" },
+        { id: crypto.randomUUID(), text: "동메달" },
+    ]);
 
-export default App
+    // crypto.randomUUID() : 고유한 식별자 생성
+
+    const onClickHandler = () => {};
+
+    return (
+        <div className="app-style">
+            {/* HTML / JSX 영역 */}
+            <h2>2024 파리 올림픽</h2>
+            <main>
+                <div className="list-style">
+                    <section>
+                        <p className="p-style">국가명</p>
+                        <form>
+                            <input type="text" min="0" placeholder="국가 이름" className="input-style" />
+                        </form>
+                    </section>
+                    <section>
+                        <p className="p-style">금메달</p>
+                        <form>
+                            <input type="number" min="0" placeholder="0" className="input-style" />
+                        </form>
+                    </section>
+                    <section>
+                        <p className="p-style">은메달</p>
+                        <form>
+                            <input type="number" min="0" placeholder="0" className="input-style" />
+                        </form>
+                    </section>
+                    <section>
+                        <p className="p-style">동메달</p>
+                        <form>
+                            <input type="number" min="0" placeholder="0" className="input-style" />
+                        </form>
+                    </section>
+                    <section>
+                        <button onClick={onClickHandler} className="Addcountry button-style">
+                            국가 추가
+                        </button>
+                        <button className="button-style">업데이트</button>
+                    </section>
+                </div>
+                <div>
+                    <ul>
+                        {lists.map((list) => (
+                            <li key={list.id}>{list.text}</li>
+                        ))}
+                    </ul>
+                </div>
+            </main>
+            <span className="span-style">아직 추가된 국가가 없습니다. 메달을 추적하세요.</span>
+        </div>
+    );
+};
+
+export default App;
