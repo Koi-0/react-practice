9440ed98e5e7a512523150088554ce01145a3b23
Koi-0 <dndud5182@gmail.com>
2025-01-23 17:44:23 +0900

    2차 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
-    const countrys = [""];
+    // 국가명, 금메달, 은메달, 동메달의 각 input 값들이 추가될 영역
+    const medalList = [];
 
-    const [country, setCountry] = useState(countrys);
+    // const [상태, 상태변경함수] = useState(초기값)
+    const [country, setCountry] = useState(medalList);
+    const [goldMedal, setGoldMedal] = useState(medalList);
+    const [silverMedal, setSilverMedal] = useState(medalList);
+    const [bronzeMedal, setBronzeMedal] = useState(medalList);
 
-    // 새로운 국가명 입력에 대한 상태 관리
+    // 새로운 입력에 대한 상태 관리
     const [newCountry, SetNewCountry] = useState("");
+    const [newGoldMedal, SetNewGoldMedal] = useState("");
+    const [newSilverMedal, SetNewSilverMedal] = useState("");
+    const [newBronzeMedal, SetNewBronzeMedal] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -16,14 +24,35 @@ const App = () => {
         }
 
         setCountry([{ id: crypto.randomUUID(), text: newCountry }, ...country]);
+        setGoldMedal([{ id: crypto.randomUUID(), text: newGoldMedal }, ...goldMedal]);
+        setSilverMedal([{ id: crypto.randomUUID(), text: newSilverMedal }, ...silverMedal]);
+        setBronzeMedal([{ id: crypto.randomUUID(), text: newBronzeMedal }, ...bronzeMedal]);
 
         SetNewCountry("");
+        SetNewGoldMedal("");
+        SetNewSilverMedal("");
+        SetNewBronzeMedal("");
         document.querySelector(".span-style").style.display = "none";
     };
 
-    // 국가명 input 태그의 변경을 감지하여 상태에 입력값 저장
+    // 국가명을 변경시키는 이벤트 핸들러
     const handleCountryInputChange = (e) => {
         SetNewCountry(e.target.value);
+    };
+
+    // 금메달을 변경시키는 이벤트 핸들러
+    const handleGoldMedalInputChange = (e) => {
+        SetNewGoldMedal(e.target.value);
+    };
+
+    // 은메달을 변경시키는 이벤트 핸들러
+    const handleSilverMedalInputChange = (e) => {
+        SetNewSilverMedal(e.target.value);
+    };
+
+    // 동메달을 변경시키는 이벤트 핸들러
+    const handleBronzeMedalInputChange = (e) => {
+        SetNewBronzeMedal(e.target.value);
     };
 
     return (
@@ -39,9 +68,9 @@ const App = () => {
                 <form onSubmit={handleSubmit}>
                     <div className="all-input-style">
                         <input type="text" placeholder="국가명" className="input-style" value={newCountry} onChange={handleCountryInputChange} />
-                        <input type="number" placeholder="0" min="0" className="input-style" />
-                        <input type="number" placeholder="0" min="0" className="input-style" />
-                        <input type="number" placeholder="0" min="0" className="input-style" />
+                        <input type="number" placeholder="0" min="0" className="input-style" value={newGoldMedal} onChange={handleGoldMedalInputChange} />
+                        <input type="number" placeholder="0" min="0" className="input-style" value={newSilverMedal} onChange={handleSilverMedalInputChange} />
+                        <input type="number" placeholder="0" min="0" className="input-style" value={newBronzeMedal} onChange={handleBronzeMedalInputChange} />
                     </div>
                     <div className="button-style">
                         <button type="submit">국가 추가</button>
@@ -52,23 +81,23 @@ const App = () => {
             <section>
                 <ul className="section-ul-style">
                     <li>
-                        {country.map((country) => (
-                            <li key={country.id}>{country.text}</li>
+                        {country.map((coun) => (
+                            <li key={coun.id}>{coun.text}</li>
                         ))}
                     </li>
                     <li>
-                        {country.map((country) => (
-                            <li key={country.id}>{country.text}</li>
+                        {goldMedal.map((gold) => (
+                            <li key={gold.id}>{gold.text}</li>
                         ))}
                     </li>
                     <li>
-                        {country.map((country) => (
-                            <li key={country.id}>{country.text}</li>
+                        {silverMedal.map((silver) => (
+                            <li key={silver.id}>{silver.text}</li>
                         ))}
                     </li>
                     <li>
-                        {country.map((country) => (
-                            <li key={country.id}>{country.text}</li>
+                        {bronzeMedal.map((bronze) => (
+                            <li key={bronze.id}>{bronze.text}</li>
                         ))}
                     </li>
                 </ul>
